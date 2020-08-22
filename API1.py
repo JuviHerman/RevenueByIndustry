b043ae6f90adc5bcc8e76b8c0c92077ed3a4bf7a
Juvin <yuvalherm@gmail.com>
2020-08-22 13:51:02 +0300

    start

--- API1.py
+++ API1.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from flask import Flask, request
 import pickle
-
+import os
 
 with open('lr_clf.pickle', 'rb') as pfile:
     clf_loaded = pickle.load(pfile)
@@ -45,4 +45,13 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    # Heroku provides environment variable 'PORT' that should be listened on by Flask
+    port = os.environ.get('PORT')
+
+    if port:
+        # 'PORT' variable exists - running on Heroku, listen on external IP and on given by Heroku port
+        app.run(host='0.0.0.0', port=int(port))
+    else:
+        # 'PORT' variable doesn't exist, running not on Heroku, presumabely running locally, run with default
+        #   values for Flask (listening only on localhost on default Flask port)
+        app.run()
